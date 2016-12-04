f67c3259633df47db5e29d9404753c0620ba8f6b
Olena <oh-i-lenka@gavatyuk.org.ua>
2016-12-04 19:05:42 +0100

    deleted comments

--- Main.java
+++ Main.java
@@ -5,8 +5,8 @@ public class Main {
     public static void main(String[] args) {
         int[] arraySample = {0,1,2,3,4,5,6,7,8};
 
-//        ArraysUtils.findEvenElements(arraySample);
-//        ArraysUtils.reverse(arraySample);
+        ArraysUtils.findEvenElements(arraySample);
+        ArraysUtils.reverse(arraySample);
 
         User[] usersArray = new User[10];
         usersArray[0] = new User(111111111, "Birgit", "Maider",4500, 10000);
@@ -18,11 +18,11 @@ public class Main {
         usersArray[7] = new User(666666666, "Xiaoly","Chu", 2100,85000);
         usersArray[9] = new User(222222222, "Petra", "Szabo", 2920, 5000);
 
-//        UserUtils.uniqueUsers(usersArray);
-//        UserUtils.usersWithContitionalBalance(usersArray, 5000);
-//        UserUtils.getUsersId(usersArray);
+        UserUtils.uniqueUsers(usersArray);
+        UserUtils.usersWithContitionalBalance(usersArray, 5000);
+        UserUtils.getUsersId(usersArray);
         UserUtils.paySalaryToUsers(usersArray);
-//        UserUtils.deleteEmptyUsers(usersArray);
+        UserUtils.deleteEmptyUsers(usersArray);
     }
 }
 
